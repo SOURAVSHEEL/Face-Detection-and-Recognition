346cf78639300beb1db56bd6eb0f10184439ef0b
Sourav Kumar Sheel <souravkrsheel99@gmail.com>
2025-06-29 15:54:42 +0530

    training loop

--- anti_spoofing/scr/data_preparation.py
+++ anti_spoofing/scr/data_preparation.py
@@ -4,12 +4,12 @@ import numpy as np
 import os
 from torchvision import transforms
 from torch.utils.data import Dataset, DataLoader
-
+from PIL import Image
 
 class AntiSpoofingDataset(Dataset):
     def __init__(self, root_dir, transform=None):
-        self.live_dir = os.path.join(root_dir,'live')
-        self.spoof_dir = os.path.join(root_dir,'spoof')
+        self.live_dir = os.path.join(root_dir, 'live')
+        self.spoof_dir = os.path.join(root_dir, 'spoof')
         self.live_images = os.listdir(self.live_dir)
         self.spoof_images = os.listdir(self.spoof_dir)
         self.transform = transform
@@ -20,7 +20,7 @@ class AntiSpoofingDataset(Dataset):
     def __getitem__(self, index):
         if index < len(self.live_images):
             img_path = os.path.join(self.live_dir, self.live_images[index])
-            label = 0  # = live
+            label = 1  # live
         else:
             img_path = os.path.join(self.spoof_dir, self.spoof_images[index - len(self.live_images)])
             label = 1 # spoof
@@ -34,20 +34,23 @@ class AntiSpoofingDataset(Dataset):
         if self.transform:
             img = self.transform(img)
 
-        return img, label
-    
+        return img, torch.tensor(label, dtype=torch.float32)
 
-
-
+# Define transforms
 transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.RandomHorizontalFlip(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    transforms.ToTensor(),  # Automatically converts [0,255] to [0,1]
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225])
 ])
 
-train_dataset = AntiSpoofingDataset(root_dir='anti_spoofing/dataset/train', transform=transform)
-train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+# Use these inside train_pipeline.py instead
+def get_dataloaders(train_root, test_root, batch_size=32):
+    train_dataset = AntiSpoofingDataset(root_dir=train_root, transform=transform)
+    test_dataset = AntiSpoofingDataset(root_dir=test_root, transform=transform)
 
-test_dataset = AntiSpoofingDataset(root_dir='anti_spoofing/dataset/test', transform=transform)
-test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
+    return train_loader, test_loader
